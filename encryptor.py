7d327b8fd993527f7538e8576bc8c2fe2c8ba545
Evgeny Abramov <abramov.ed@phystech.edu>
2019-04-04 16:00:00 +0300

    Исправил ошибки

--- encryptor.py
+++ encryptor.py
@@ -9,15 +9,15 @@ from text_manager import get_stream
 def get_next_symbol(symbol, step, is_encoding=True):
     if not is_encoding:
         step = -step
+    if symbol not in string.ascii_letters:
+        return symbol
     if symbol in string.ascii_uppercase:
         lower_bound = ord('A')
         upper_bound = ord('Z')
-        return chr(lower_bound + (ord(symbol) - lower_bound + step) % (upper_bound - lower_bound + 1))
-    elif symbol in string.ascii_lowercase:
+    else:
         lower_bound = ord('a')
         upper_bound = ord('z')
-        return chr(lower_bound + (ord(symbol) - lower_bound + step) % (upper_bound - lower_bound + 1))
-    return symbol
+    return chr(lower_bound + (ord(symbol) - lower_bound + step) % (upper_bound - lower_bound + 1))
 
 
 def caesar(data, key, is_encoding):
@@ -32,25 +32,13 @@ def vigenere(data, key, is_encoding):
     return data
 
 
-def encode(cipher, key, input_filename, output_filename):
+def process(cipher, key, input_filename, output_filename, is_encoding):
     with get_stream(input_filename, 'r') as input_file:
         data = list(input_file.read())
     if cipher == 'caesar':
-        data = caesar(data, int(key), is_encoding=True)
+        data = caesar(data, int(key), is_encoding)
     else:
-        data = vigenere(data, key, is_encoding=True)
-    data = ''.join(data)
-    with get_stream(output_filename, 'w') as output_file:
-        output_file.write(data)
-
-
-def decode(cipher, key, input_filename, output_filename):
-    with get_stream(input_filename, 'r') as input_file:
-        data = list(input_file.read())
-    if cipher == 'caesar':
-        data = caesar(data, int(key), is_encoding=False)
-    else:
-        data = vigenere(data, key, is_encoding=False)
+        data = vigenere(data, key, is_encoding)
     data = ''.join(data)
     with get_stream(output_filename, 'w') as output_file:
         output_file.write(data)
@@ -78,33 +66,34 @@ def train(text_filename, model_filename):
         json.dump(frequency, model_file)
 
 
+def get_difference(step, model_frequency, frequency):
+    result = 0
+    for fake_letter in frequency.keys():
+        result += abs(model_frequency[get_next_symbol(fake_letter, step)] - frequency[fake_letter])
+    return result
+
+
 def hack(input_filename, output_filename, model_filename):
     with open(model_filename, 'r') as model_file:
         model_frequency = json.load(model_file)
     with get_stream(input_filename, 'r') as input_file:
         data = list(input_file.read())
     frequency = count_frequency(data)
-    min_diff = len(string.ascii_lowercase)
-    min_step = 0
-    for step in range(len(string.ascii_lowercase)):
-        diff = 0
-        for fake_letter in frequency.keys():
-            diff += abs(model_frequency[get_next_symbol(fake_letter, step)]
-                        - frequency[fake_letter])
-        if diff < min_diff:
-            min_step = step
-            min_diff = diff
+    min_step = min([step for step in range(alphabet_length)],
+                   key=lambda step: get_difference(step, model_frequency, frequency))
     data = caesar(data, min_step, is_encoding=True)
     data = ''.join(data)
     with get_stream(output_filename, 'w') as output_file:
         output_file.write(data)
 
 
+alphabet_length = len(string.ascii_lowercase)
+
 args = parser.command_parser.parse_args()
 if args.method == 'encode':
-    encode(args.cipher, args.key, args.input_file, args.output_file)
+    process(args.cipher, args.key, args.input_file, args.output_file, is_encoding=True)
 elif args.method == 'decode':
-    decode(args.cipher, args.key, args.input_file, args.output_file)
+    process(args.cipher, args.key, args.input_file, args.output_file, is_encoding=False)
 elif args.method == 'train':
     train(args.text_file, args.model_file)
 elif args.method == 'hack':
